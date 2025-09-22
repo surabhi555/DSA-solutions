cc2c0fcf49bd924026aec2c22a5ce8e7f30a52e7
Surabhi Pednekar <pednekarsurabhi555@gmail.com>
2025-09-22 22:03:54 +0530

    day 12 completed!

---
+++ BinarySearch/splitArrayLargestSum.cpp
@@ -0,0 +1,37 @@
+https://leetcode.com/problems/split-array-largest-sum/
+class Solution {
+public:
+    int countPartitions(vector<int> &a, int maxSum) {
+    int n = a.size(); //size of array.
+    int partitions = 1;
+    long long subarraySum = 0;
+    for (int i = 0; i < n; i++) {
+        if (subarraySum + a[i] <= maxSum) {
+            //insert element to current subarray
+            subarraySum += a[i];
+        }
+        else {
+            //insert element to next subarray
+            partitions++;
+            subarraySum = a[i];
+        }
+    }
+    return partitions;
+}
+    int splitArray(vector<int>& nums, int k) {
+         int low = *max_element(nums.begin(), nums.end());
+        int high = accumulate(nums.begin(), nums.end(), 0);
+        while (low <= high) {
+        int mid = (low + high) / 2;
+        int partitions = countPartitions(nums, mid);
+        if (partitions > k) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return low;
+
+    }
+};
